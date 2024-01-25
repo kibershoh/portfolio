61b5458edb63f648eff5b776750885adbf9de04d
Oybek <saminovo150947@gmail.com>
2024-01-26 03:35:45 +0500

    add projects

--- src/Components/Experience/index.jsx
+++ src/Components/Experience/index.jsx
@@ -1,6 +1,8 @@
 import search from '../.././assets/search.jpg'
 import giotto from '../.././assets/giotto.jpg'
 import foods from '../.././assets/foods.jpg'
+import noteapp from '../.././assets/noteapp.png'
+import xurmo from '../.././assets/xurmo.png'
 import { Link } from "react-router-dom"
 const Experience = () => {
   return (
@@ -19,6 +21,16 @@ const Experience = () => {
         
       <img className="w-3/4 h-3/4 " src={giotto} alt="" />
       <button className="bg-blue-600 p-2 rounded text-white hover:scale-110 duration-1000 mt-10"><Link target="_blank" to={'https://giotto1.netlify.app/'}>Ko&apos;rish</Link></button>
+     </div>
+     <div className="border border-blue-600 rounded p-10">
+        
+      <img className="w-3/4 h-3/4 " src={noteapp} alt="" />
+      <button className="bg-blue-600 p-2 rounded text-white hover:scale-110 duration-1000 mt-10"><Link target="_blank" to={'https://shopping-sooty-phi.vercel.app/'}>Ko&apos;rish</Link></button>
+     </div>
+     <div className="border border-blue-600 rounded p-10">
+        
+      <img className="w-3/4 h-3/4 " src={xurmo} alt="" />
+      <button className="bg-blue-600 p-2 rounded text-white hover:scale-110 duration-1000 mt-10"><Link target="_blank" to={'https://note2-app.vercel.app/'}>Ko&apos;rish</Link></button>
      </div>
       
     </div>
